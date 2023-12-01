db5b4e0fe6dc167addf1f6e04da98a428b1aba67
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-01 12:58:26 +0100

    add pm2 config and simplify php dockerfile

---
+++ server/client/public/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dev',
+      exec_mode: 'cluster',
+      instances: '3',
+      cwd: '/var/www/html/starter/server/public/',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start --hostname localhost --port 3030',
+      interpreter_args: '',
+      merge_logs: true,
+      log_date_format: "HH:mm:ss.SSS"
+    }
+  ]
+};
