60ef3ef1ef3ef60906bab3389ebbccd933725d07
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-11 19:03:42 +0100

    change esocystem config

--- server/client/public/ecosystem.config.cjs
+++ server/client/public/ecosystem.config.cjs
@@ -5,11 +5,8 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '3',
       cwd: '/var/www/html/thestarter.net/current/client/public',
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start --hostname localhost --port 3030',
-      interpreter_args: '',
-      merge_logs: true,
-      log_date_format: "HH:mm:ss.SSS"
+      script: './.output/server/index.mjs',
+      port: '3030'
     }
   ]
 };
