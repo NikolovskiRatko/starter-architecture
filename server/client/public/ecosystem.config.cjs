c52edfad481bf1ba98a31a0944095b0e4d1a2836
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-12 13:07:24 +0100

    fix path of pm2 config

--- server/client/public/ecosystem.config.cjs
+++ server/client/public/ecosystem.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
       name: 'dev',
       exec_mode: 'cluster',
       instances: '3',
-      cwd       : './current',
       script: './.output/server/index.mjs',
       port: '3030'
     }
