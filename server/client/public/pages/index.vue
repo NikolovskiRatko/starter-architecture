7a030d4aa66bb2736f6d997d08e60f32276f6750
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-14 19:05:24 +0100

    test failed deployment to check rollback function

--- server/client/public/pages/index.vue
+++ server/client/public/pages/index.vue
@@ -4,7 +4,7 @@
             
         </div>
         <div class="container-fluid">
-            <h1 class="offset-md-2">Home Page 1.4</h1>
+            <h1 class="offset-md-2">Home Page 1.5</h1>
         </div>
     </div>
 </template>
