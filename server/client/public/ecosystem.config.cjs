5c4c4f5a1d89e07ba10fd653072a20fb73143032
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-14 15:48:19 +0100

    test pm2 config

--- server/client/public/ecosystem.config.cjs
+++ server/client/public/ecosystem.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
       exec_mode: 'cluster',
       instances: '3',
       script: './.output/server/index.mjs',
-      port: '3030'
+      port: '3030',
+      args: 'start',
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+      },
     }
   ]
 };
