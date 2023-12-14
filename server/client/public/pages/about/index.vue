16bedd387260f2f443a339519bd31d6dc85f8485
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2023-12-14 16:11:22 +0100

    test deployment

--- server/client/public/pages/about/index.vue
+++ server/client/public/pages/about/index.vue
@@ -4,7 +4,7 @@
             
         </div>
         <div class="container-fluid jumbotron_wrapper">
-            <h1 class="offset-md-2">About Us Page 1.1</h1>
+            <h1 class="offset-md-2">About Us Page 1.2</h1>
         </div>
     </div>
 </template>
