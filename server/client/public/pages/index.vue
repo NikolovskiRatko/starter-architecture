85355906a58cead523569272a8df92f89c6eabbc
Ratko Nikolovski <nikolovski.ratko@gmail.com>
2024-01-10 15:08:35 +0100

    add a separate bundler config for production

--- server/client/public/pages/index.vue
+++ server/client/public/pages/index.vue
@@ -4,7 +4,7 @@
             
         </div>
         <div class="container-fluid">
-            <h1 class="offset-md-2">Home Page 1.2</h1>
+            <h1 class="offset-md-2">Home Page 1.3</h1>
         </div>
     </div>
 </template>
