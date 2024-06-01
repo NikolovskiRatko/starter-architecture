b76a26e15adae1323e14cf43381ec8c426a0b210
Dimitar Pashovski <dipa@boozt.com>
2024-06-01 09:48:52 +0200

    update api response types

--- server/client/admin/src/types/api.ts
+++ server/client/admin/src/types/api.ts
@@ -1,21 +1,23 @@
-export type ApiResponse<T = void> = {
-  success?: boolean;
-  message?: string;
-  data: T;
-  code?: number;
-};
+export interface EloquentModelCommonFields {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
 
-export type PaginatedApiResponse<T> = ApiResponse<T> & {
-  pagination: {
-    total: number;
-    count: number;
-    currentPage: number;
-    lastPage: number;
-    limit: number;
-    options: {
-      path: string;
-      pageName: string;
-    };
-    dataLength: number;
+export interface ApiResponsePagination {
+  total: number;
+  count: number;
+  currentPage: number;
+  lastPage: number;
+  limit: number;
+  options: {
+    path: string;
+    pageName: string;
   };
+  dataLength: number;
+}
+
+export type PaginatedApiResponse<T> = {
+  data: T[];
+  pagination: ApiResponsePagination;
 };
