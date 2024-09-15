8376c78e436c92082cc54ea83a05321eeb8acb61
Dimitar Pashovski <dipa@boozt.com>
2024-09-15 10:25:44 +0200

    fix useUserROles

--- server/client/admin/src/composables/vue-query/useUserRoles.ts
+++ server/client/admin/src/composables/vue-query/useUserRoles.ts
@@ -1,11 +1,11 @@
+import axios from "axios";
 import { useQuery } from "@tanstack/vue-query";
-import { get } from "@/services/HTTP";
 
 export const useUserRoles = () => {
   return useQuery({
     queryKey: ["roles"],
     queryFn: async () => {
-      const data = await get("user/roles/get");
+      const data = await axios.get("user/roles/get");
       return data.data;
     },
   });
