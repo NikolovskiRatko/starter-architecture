f729683d372e774603a4f43acb195b3749d243df
Dimitar Pashovski <epashata@gmail.com>
2024-10-28 00:05:18 +0100

    create navigation translations

--- app/client/admin/src/locales/en/index.ts
+++ app/client/admin/src/locales/en/index.ts
@@ -1,6 +1,7 @@
 import admin from "./admin.json";
 import buttons from "./buttons.json";
 import strings from "./strings.json";
+import { navigation } from "@/modules/navigation/locales";
 import { users } from "@/modules/users/locales";
 
-export { strings, buttons, users, admin };
+export { strings, buttons, users, admin, navigation };
