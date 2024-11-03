55f10360792386de6264f53914a9c2d8d9ab62d6
Dimitar Pashovski <epashata@gmail.com>
2024-11-03 12:57:31 +0100

    Styled up the menu items

--- server/client/admin/src/modules/navigation/components/MenuItems.vue
+++ server/client/admin/src/modules/navigation/components/MenuItems.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
-  import { computed } from "vue";
+  import { computed, type Ref } from "vue";
   import { useNavigationMenu } from "../composables";
+  import MenuItem from "./MenuItem.vue";
+  import { useBEMBuilder } from "@/helpers";
+  import "./MenuItems.scss";
 
   interface MenuItemsProps {
     menuId: Ref<string>;
   }
+
+  const [block, element] = useBEMBuilder("menu-items");
 
   const { menuId } = defineProps<MenuItemsProps>();
   const id = computed<number>(() => Number(menuId));
@@ -13,11 +18,9 @@
   const items = computed(() => data.value?.items ?? []);
 </script>
 <template>
-  <div>
-    <ul>
-      <li v-for="item in items" v-bind:key="item.id">
-        {{ item.label }}
-      </li>
+  <div :class="block">
+    <ul :class="element('list').value">
+      <MenuItem v-for="item in items" v-bind:key="item.id" :item="item" />
     </ul>
     <p v-if="!items.length">There is no items assigned to this menu</p>
   </div>
