ce3d70d0407a362761553a533a1199737aae4609
Dimitar Pashovski <epashata@gmail.com>
2024-11-03 12:57:31 +0100

    Styled up the menu items

---
+++ server/client/admin/src/modules/navigation/components/MenuItem.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  import { computed } from "vue";
+  import type { NavigationMenuItem } from "../types";
+  import { useBEMBuilder } from "@/helpers";
+  import "./MenuItem.scss";
+
+  const [block, element] = useBEMBuilder("menu-item");
+
+  const { item } = defineProps<{ item: NavigationMenuItem }>();
+
+  const type = computed(() => (item.navigation_id ? "Internavl" : "External"));
+</script>
+<template>
+  <li :class="block">
+    <span>{{ item.label }}</span>
+    <span>{{ type }}</span>
+  </li>
+</template>
