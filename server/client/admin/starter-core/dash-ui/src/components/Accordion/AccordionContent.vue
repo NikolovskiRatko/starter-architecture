4ad8d923e3c690200972dd748f5495ab09375418
Dimitar Pashovski <epashata@gmail.com>
2024-11-04 08:40:47 +0100

    add onchange in accordions

--- server/client/admin/starter-core/dash-ui/src/components/Accordion/AccordionContent.vue
+++ server/client/admin/starter-core/dash-ui/src/components/Accordion/AccordionContent.vue
@@ -12,6 +12,10 @@
   const [block, element] = useBEMBuilder("accordion-content");
 
   const { isLoading = false } = defineProps<AccordionContentProps>();
+  const emit = defineEmits<{
+    (e: "onAccordionChange", accordionId: string): void;
+  }>();
+
   const accordions: AccordionItem[] = reactive([]);
   const activeAccordion: Ref<string> = ref("");
 
@@ -19,14 +23,16 @@
     accordions.push(accordion);
   });
 
-  provide(SetActiveAccordionKey, (id) => {
+  provide(SetActiveAccordionKey, (id: string) => {
     activeAccordion.value = id;
+    emit('onAccordionChange', id);
   });
 
   provide(ActiveAccordionIdKey, activeAccordion);
 
   onMounted(() => {
     activeAccordion.value = accordions[0].id;
+    emit('onAccordionChange', accordions[0].id);
   });
 </script>
 <template>
