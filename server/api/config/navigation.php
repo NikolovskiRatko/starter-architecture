4b66a6b6e2ca0e14e954178ad24f2e7337413bf7
Dimitar Pashovski <epashata@gmail.com>
2024-12-11 23:09:51 +0100

    remove model type

--- server/api/config/navigation.php
+++ server/api/config/navigation.php
@@ -3,7 +3,6 @@
 return [
     'model_types' => [
         'User' => 'App\Applications\User\Model\User',
-        'Page' => 'App\Applications\Builder\Model\Page',
         // Add more types here
     ],
 ];
