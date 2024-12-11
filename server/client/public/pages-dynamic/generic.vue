eb34c0a660710fe371c8b9582a3b8e129ae68d6b
Dimitar Pashovski <epashata@gmail.com>
2024-12-11 22:41:36 +0100

    extend pages with dynamic routes

---
+++ server/client/public/pages-dynamic/generic.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const { id, type } = route.meta;
+</script>
+<template>
+  <Jumbotron>
+    <h1>Generic dynamic page</h1>
+    {{type}} - {{id}}
+  </Jumbotron>
+</template>
