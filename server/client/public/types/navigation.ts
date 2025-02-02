713057faa517d198843b5a77ad97ad6a0d411e7a
Dimitar Pashovski <epashata@gmail.com>
2025-02-02 12:19:43 +0100

    remove garbage files from rebase

--- server/client/public/types/navigation.ts
+++
@@ -1,9 +0,0 @@
-export interface DynamicNavigationRoute {
-  title: string;
-  slug: string;
-  path: string;
-  content_type: string | null;
-  content: unknown;
-}
-
-export type DynamicNavigationRoutes = DynamicNavigationRoute[];
