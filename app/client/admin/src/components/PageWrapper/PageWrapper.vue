a74b4eec0e38898db2e531bd6643c94062720c8b
Dimitar Pashovski <epashata@gmail.com>
2025-04-09 23:15:15 +0200

    extend Page wrapper with size props

--- app/client/admin/src/components/PageWrapper/PageWrapper.vue
+++ app/client/admin/src/components/PageWrapper/PageWrapper.vue
@@ -3,8 +3,11 @@
   import { PAGE_WRAPPER_SLOTS } from "./constants";
   import type { PageWrapperSlot } from "./types";
   import { useBEMBuilder } from "@/helpers";
-
   import "./PageWrapper.scss";
+
+  const { size } = defineProps<{
+    size?: "small" | "medium" | "large";
+  }>();
 
   const slots = useSlots();
   const hasSubheaderSlot = computed(() => {
@@ -31,7 +34,18 @@
       </div>
     </div>
     <div :class="element('content').value">
-      <slot></slot>
+      <div
+        :class="
+          element(
+            'content-inner',
+            ref({
+              [`${size}`]: !!size,
+            }),
+          ).value
+        "
+      >
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
