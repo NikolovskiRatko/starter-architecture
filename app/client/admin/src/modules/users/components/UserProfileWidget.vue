7221db0c4cc0802e74fe9739c1fa80d48e104c4f
Dimitar Pashovski <epashata@gmail.com>
2025-04-12 13:19:40 +0200

    Create User profile widget component

---
+++ app/client/admin/src/modules/users/components/UserProfileWidget.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+  import { useI18n } from "vue-i18n";
+  import { MY_PROFILE_WIDGET_LINKS } from "../constants";
+  import UserRole from "./UserRole.vue";
+  import { useAuth } from "@/composables";
+  import { PortletComponent, PortletBody } from "@starter-core/dash-ui/src";
+  import "./UserProfileWidget.scss";
+
+  const { user } = useAuth();
+  const { t } = useI18n();
+
+  const avatarSource = computed(() => {
+    if (user?.value?.avatar_thumbnail) {
+      return user.value.avatar_thumbnail;
+    }
+
+    return null;
+  });
+
+  const fullName = computed(() => {
+    if (user?.value) {
+      return `${user.value.first_name} ${user.value.last_name}`;
+    }
+
+    return null;
+  });
+
+  const widgetInfo = [
+    {
+      label: t("users.email.label"),
+      value: user.value.email,
+    },
+    {
+      label: "Phone",
+      value: "",
+    },
+    {
+      label: "Location",
+      value: "",
+    },
+  ];
+</script>
+<template>
+  <PortletComponent>
+    <PortletBody>
+      <div class="user-profile-widget user-profile-widget--user-profile-1">
+        <div class="user-profile-widget__head">
+          <div class="user-profile-widget__media">
+            <img v-if="avatarSource" :src="avatarSource" alt="image" />
+          </div>
+          <div class="user-profile-widget__content">
+            <div class="user-profile-widget__section">
+              <a href="#" class="user-profile-widget__username">
+                {{ fullName }}
+                <i class="flaticon2-correct kt-font-success"></i>
+              </a>
+              <span class="user-profile-widget__subtitle">
+                <UserRole :userRoleId="user.role" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="user-profile-widget__body">
+          <div class="user-profile-widget__content">
+            <div
+              v-for="info in widgetInfo"
+              :key="info.label"
+              class="user-profile-widget__info"
+            >
+              <span class="user-profile-widget__label">{{ info.label }}:</span>
+              <a href="#" class="user-profile-widget__data">{{ info.value }}</a>
+            </div>
+          </div>
+          <div class="user-profile-widget__items">
+            <router-link
+              v-for="{ label, routeName, Icon } in MY_PROFILE_WIDGET_LINKS"
+              :key="label"
+              :to="{ name: routeName }"
+              active-class="user-profile-widget__item--active"
+              class="user-profile-widget__item"
+            >
+              <span class="user-profile-widget__section">
+                <span class="user-profile-widget__icon">
+                  <component :is="Icon"></component>
+                </span>
+                <span class="user-profile-widget__desc"> {{ label }} </span>
+              </span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </PortletBody>
+  </PortletComponent>
+</template>
