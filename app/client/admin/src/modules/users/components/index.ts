051dcfcfe0988e8e726ff916d3ae641924045719
Dimitar Pashovski <epashata@gmail.com>
2025-04-13 16:47:40 +0200

    remove missing export

--- app/client/admin/src/modules/users/components/index.ts
+++ app/client/admin/src/modules/users/components/index.ts
@@ -1,5 +1,4 @@
 export { default as UsersDatatable } from "./UsersDatatable.vue";
-export { default as UserFormBasicInfoTab } from "./UserFormBasicInfoTab.vue";
 export { default as UserFormPasswordTab } from "./UserFormPasswordTab.vue";
 export { default as ChangePasswordForm } from "./ChangePasswordForm.vue";
 export { default as UserProfileWidget } from "./UserProfileWidget.vue";
