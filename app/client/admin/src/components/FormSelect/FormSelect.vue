4bd6ac5af868d7d5a878cfc477100d69c23b3824
Dimitar Pashovski <epashata@gmail.com>
2025-06-28 19:06:17 +0200

    run lint fix

--- app/client/admin/src/components/FormSelect/FormSelect.vue
+++ app/client/admin/src/components/FormSelect/FormSelect.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import VueSelect from "vue-select";
-import "vue-select/dist/vue-select.css";
-import './FormSelect.scss';
+  import VueSelect from 'vue-select';
+  import 'vue-select/dist/vue-select.css';
+  import './FormSelect.scss';
 
+  defineProps<{
+    modelValue: string | string[] | null;
+    options: (string | { label: string; value: string })[];
+    placeholder?: string;
+    multiple?: boolean;
+  }>();
 
-defineProps<{
-  modelValue: string | string[] | null;
-  options: (string | { label: string; value: string })[];
-  placeholder?: string;
-  multiple?: boolean;
-}>();
-
-const emit = defineEmits<{
-  (e: 'update:modelValue', value: string | string[] | null): void;
-}>();
+  const emit = defineEmits<{
+    (e: 'update:modelValue', value: string | string[] | null): void;
+  }>();
 </script>
 
 <template>
   <VueSelect
-      :model-value="modelValue"
-      :options="options"
-      :placeholder="placeholder"
-      :multiple="multiple"
-      taggable
-      :clearable="false"
-      class="dui-form-select"
-      @update:modelValue="emit('update:modelValue', $event)"
+    :model-value="modelValue"
+    :options="options"
+    :placeholder="placeholder"
+    :multiple="multiple"
+    taggable
+    :clearable="false"
+    class="dui-form-select"
+    @update:modelValue="emit('update:modelValue', $event)"
   />
 </template>
