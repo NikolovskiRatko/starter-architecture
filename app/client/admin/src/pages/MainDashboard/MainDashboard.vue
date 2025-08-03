f297234fafa0bb65777c77c9fbf2b2afa90f173f
Dimitar Pashovski <epashata@gmail.com>
2025-08-03 09:19:52 +0200

    clean up dashboard

--- app/client/admin/src/pages/MainDashboard/MainDashboard.vue
+++ app/client/admin/src/pages/MainDashboard/MainDashboard.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-  import { IconAirpods, IconChartpie, IconDollar, IconLibrary } from '@starter-core/icons';
+  import { IconChartpie, IconDollar, IconLibrary } from '@starter-core/icons';
   import { ref, onMounted } from 'vue';
   import { PageWrapper } from '@/components';
   import { useRootStore } from '@/store/root';
-  import {
-    PortletComponent,
-    PortletBody,
-    PortletHead,
-    PortletHeadLabel,
-    ContentLoader,
-    AccordionContent,
-    AccordionItem,
-    DashButton,
-  } from '@starter-core/dash-ui/src';
+  import { AccordionContent, AccordionItem } from '@starter-core/dash-ui/src';
 
   // const categories = ref([]);
   const isLoading = ref(false);
@@ -35,41 +26,43 @@
 <template>
   <PageWrapper>
     <div class="row">
-      <div class="col-md-4 mb-4">
-        <PortletComponent isBordered>
-          <PortletHead>
-            <PortletHeadLabel>
-              Content loader
-              <IconAirpods size="32" />
-            </PortletHeadLabel>
-          </PortletHead>
-          <PortletBody>
-            <ContentLoader />
-          </PortletBody>
-        </PortletComponent>
-      </div>
-      <div class="col-md-4"></div>
-      <div class="col-md-4"></div>
-      <div class="col-md-3">
+      <div class="col-md-6 offset-3">
         <AccordionContent>
-          <AccordionItem label="Product inventory" id="product-inventory" :icon="IconChartpie">
-            Vero laborum esse debitis libero veniam ullam placeat molestias deleniti distinctio magnam? In, odio alias? Possimus
-            labore delectus recusandae.
+          <AccordionItem label="Integration and Interoperability" id="integration-interoperability" :icon="IconChartpie">
+            Our architecture seamlessly integrates two distinct frontend applications backed by a single backend API to provide a
+            comprehensive user experience. This integrated approach allows us to maintain separation between administrative tasks
+            and public-facing content delivery, ensuring a responsive and SEO-friendly web application for all users.
           </AccordionItem>
-          <AccordionItem label="Order statistics" id="order-statistics" :icon="IconLibrary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, aut molestiae.
+
+          <AccordionItem label="Admin Panel (Vue.js Single Page Application)" id="admin-panel" :icon="IconLibrary">
+            The integration of Laravel and Vue.js begins with Laravel serving as the foundation. Laravel bootstraps the Vue.js
+            Single Page Application (SPA) by providing the essential web routes, including user authentication and authorization
+            mechanisms. Admin users benefit from a feature-rich Admin Panel built using Vue.js. This panel, hosted within the same
+            domain as the Laravel API, offers an intuitive interface for managing and overseeing various aspects of the
+            application.
           </AccordionItem>
-          <AccordionItem label="eCommerce reports" id="ecommerce-reports" :icon="IconDollar">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, aut molestiae.
+
+          <AccordionItem label="Public Content (Nuxt Server Side Rendering)" id="public-content" :icon="IconDollar">
+            Recognizing the importance of a distinct, public-facing interface, we've provisioned a separate front-end built on
+            Nuxt, leveraging server-side rendering (SSR). Nuxt, powered by Node.js and managed by pm2, optimizes content delivery
+            for public users. This architecture not only provides swift and efficient content rendering but also excels in search
+            engine optimization (SEO). The Nuxt SSR application utilizes the Laravel application as an API, ensuring data
+            consistency and efficient communication between the frontend and backend.
+          </AccordionItem>
+
+          <AccordionItem label="Infrastructure (Docker, Terraform, Ansible, Jenkins)" id="infrastructure" :icon="IconChartpie">
+            Our infrastructure seamlessly integrates with the application layers. For local development, Docker Compose is
+            employed to create a Dockerized environment consisting of Apache/PHP, MySQL, Node.js, and Redis containers. This
+            environment closely mirrors our production setup, ensuring consistency during development.
+            <br /><br />
+            On live servers, Terraform orchestrates the creation and configuration of servers and DNS records. Ansible Playbooks
+            are then used for provisioning these servers. Two servers are created: one hosts the application, while the other, a
+            Jenkins server, manages our CI/CD processes. The Jenkins server is tightly integrated with GitHub, automating the
+            creation of multipipeline jobs. Each project defines its pipelines, including a Dockerized build environment via
+            Docker Compose. These pipelines manage the build stage and incorporate Ansible Playbooks for zero downtime deployments
+            and rollback mechanisms in case of failures.
           </AccordionItem>
         </AccordionContent>
-      </div>
-      <div class="col-md-6">
-        <DashButton>Jajca goli</DashButton>
-        <DashButton theme="secondary">Jajca goli</DashButton>
-        <DashButton theme="success">Jajca goli</DashButton>
-        <DashButton theme="info">Jajca goli</DashButton>
-        <DashButton theme="danger">Jajca goli</DashButton>
       </div>
     </div>
   </PageWrapper>
